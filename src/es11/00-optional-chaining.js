4601b14a6a1dca7059e7418f8075e648802e3f15
oscar.franco <ofrubio0@gmail.com>
2023-03-15 12:20:59 -0500

    Se agregan caracteristicas de es11

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,11 @@
+const users = {
+    gndx:{
+        country: 'Mx'
+    },
+    ana:{
+        country: 'CO'
+    }
+}
+
+console.log(users.gndx)
+console.log(users?.bebeloper?.country)
---
+++ src/es11/01-big-int.js
@@ -0,0 +1,4 @@
+const abigNumber = 8464846465483148744648754584654
+const anotherBigNumber = BigInt(8464846465483148744648754584654)
+console.log(abigNumber)
+console.log(anotherBigNumber)
